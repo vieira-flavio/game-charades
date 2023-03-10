b06eb2d4f2c862af994c94ab1531006bcc4a37c8
print-flavio <flavio@vghcorp.com>
2023-03-10 15:07:14 -0800

    Creating choice for user to select version.

--- charades.py
+++ charades.py
@@ -1,23 +1,60 @@
 # Modules:
 import random
-# Welcoming str and asking player if they want to start playing.
-print("\n Welcome to Charades!!!\n")
-userAsk = str(input("\n Shall we begin (y/n)?"))
-# List of players:
+
+# Global variables:
+
+# strings:
+game = "Charades"
+user_ask_start = "Shall we begin (y/n)?"
+user_welcome = "Welcome Players!"
+user_ask_version_of_game = "Please select from the following list the version of" 
+user_ask_game_type = user_ask_version_of_game + game
+
+# lists:
+charades_version_list = ['PG', 'R-RATED']
 playerList = []
+
+# Welcoming user and listing version of Charades user wants to play.
+print(f"\n{user_welcome} \n\n{user_ask_version_of_game} {game} you would like to play by inputing the number associated with your choice.\n")
+for i in charades_version_list:
+     print(charades_version_list.index(i) +1, end=" ")
+     print(" ", i)
+
+# Choice outcome.
+answer = (input("\nWhats it going to be?:"))
+if answer == "1":
+     print("\nGood Choice,", charades_version_list[0]," it is!")
+elif answer == "2":
+     print("\nAwesome!", charades_version_list[1]," it is!")
+
+
+
+
+
+
+
+
+user_ask_start = (input("\n Are you ready (y/n)?"))
+
+
 # Player answers y to begin playing:
-if userAsk == "y":
+if user_ask_start == "y":
     userNun=int(input("\n Number of players?\n> "))
     while len(playerList) != userNun:
-        userNames=input("Please input name of player number {}/{}\n> ".format(len(playerList)+1, userNun))
+        userNames=input(f"Please input name of player {len(playerList)+1}/{userNun} :\n")
         playerList.append(userNames)
+
 # Player answers n to begin game:
-elif userAsk == "n":
+elif user_ask_start == "n":
     print("\n\n Goodbye \n")
     exit()
+
 # Player enters other input:   
 else:
-     print("\n Want to try that again? I've got all day. y / n? \n")
+     print("\n Want to try that again? I've got all day.\n")
+     exit()
+
+    
 
 # print("\nIt's your turn:" , random.choice(playerList))
 playerIndex = 0
