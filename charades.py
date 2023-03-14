f035f3d420402514297985939ea38277a44ff024
print-flavio <flavio@vghcorp.com>
2023-03-14 14:46:45 -0700

    Switching statement.

--- charades.py
+++ charades.py
@@ -4,32 +4,28 @@ import random
 # Global variables:
 
 # strings:
-game = "Charades"
 user_ask_start = "Shall we begin (y/n)?"
-user_welcome = "Welcome Players!"
-user_ask_version_of_game = "Please select from the following list the version of" 
-user_ask_game_type = user_ask_version_of_game + game
 
 # lists:
 charades_version_list = ['PG', 'R-RATED']
 playerList = []
 
 # Welcoming user and listing version of Charades user wants to play.
-print(f"\n{user_welcome} \n\n{user_ask_version_of_game} {game} you would like to play by inputing the number associated with your choice.\n")
+print("\nWelcome Players! \n\nPlease select from the following list the version of Charades you would like to play by inputing the number associated with your choice.\n")
 for i in charades_version_list:
      print(charades_version_list.index(i) +1, end=" ")
      print(" ", i)
 
-# Choice outcome.
+# Player choose outcom.
 answer = (input("\nWhats it going to be?:"))
-if answer == "1":
-     print("\nGood Choice,", charades_version_list[0]," it is!")
-elif answer == "2":
-     print("\nAwesome!", charades_version_list[1]," it is!")
-
-
+match answer:
+     case "1":
+          print(f"\nGood Choice {charades_version_list[0]} it is!.")
+     case "2":
+          print(f"\nGood Choice {charades_version_list[1]} it is!.")     
+          
+# Start of game.
 user_ask_start = (input("\nAre you ready (y/n)?"))
-
 
 # Player answers y to begin playing:
 if user_ask_start == "y":
@@ -43,14 +39,6 @@ elif user_ask_start == "n":
     print("\n\n Goodbye \n")
     exit()
 
-# Player enters other input:   
-else:
-     print("\n Want to try that again? I've got all day.\n")
-     exit()
-
-    
-
-# print("\nIt's your turn:" , random.choice(playerList))
 playerIndex = 0
 while True:
     WORDS = ["airplane", "boat", "baby", "Phone", "Toothbrush", "Hammer", "Shoelaces", "Flashlight", "Clown", "Mime", "Whale", "Kangaroo", "Monkey", "Rooster", "Cockroach", "Bartender", "Cow", "Bullfighter", "Yard sale", "Penguin"]
@@ -59,9 +47,10 @@ while True:
     correct = word
     jumble = ""
     
-    print("\n", playerList[playerIndex], "It's your turn:") 
-    print("\n The word / phrase to act out is:\n\n", word.title())
+    print(f"\n {playerList[playerIndex]}, it's your turn:") 
+    print(f"\n The word / phrase to act out is:\n\n{word.title()}")
     
+
     guess = input("\n Who guessed this correctly? ")
     playerIndex = (playerIndex+1) % len(playerList)
     
@@ -76,7 +65,5 @@ while True:
     else:
         print("Goodbye")
         break
-
-
 
 input("\n\nPress the enter key to exit")
